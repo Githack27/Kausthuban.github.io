9bc3638497e43ef701f7ec5fb77751e3da7c5c8a
Kausthuban M <arceusxy2003@gmail.com>
2024-08-06 14:37:11 +0530

    Completed Files

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}  
+
+function navigateToLinkedIn(){
+    return window.location.href="https://linkedin.com/in/kausthuban-m-ba255b217"
+}
+
+function navigateToGithub(){
+    return window.location.href="https://github.com/GitHack27"
+}
